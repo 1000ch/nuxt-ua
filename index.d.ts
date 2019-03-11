ef0656fb9eaa20c5c0bf259fac696a1b9a6be6cc
1000ch <shogo.sensui@gmail.com>
2019-03-11 16:38:04 +0900

    refactor: add namespace

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+declare namespace NuxtUA {
+  export interface OS {
+    architecture: string;
+    family: string;
+    toString(): string;
+    version: string;
+  }
+
+  export interface Platform {
+    description: string;
+    layout: string;
+    manufacturer: string;
+    name: string;
+    os: OS;
+    parse(): Platform;
+    prerelease: string;
+    product: string;
+    toString(): string;
+    ua: string;
+    version: string;
+  }
+
+  export interface UserAgent {
+    platform: Platform;
+    is: {
+      readonly Chrome: boolean;
+      readonly Firefox: boolean;
+      readonly Safari: boolean;
+      readonly Android: boolean;
+      readonly iOS: boolean;
+      readonly macOS: boolean;
+      readonly Windows: boolean;
+    };
+  }
+}
+
+declare module 'vue/types/vue' {
+  export interface Vue {
+    $ua: UserAgent;
+  }
+}
