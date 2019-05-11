a63ccfdf7021781e40c79355bf944090f02fd25c
1000ch <shogo.sensui@gmail.com>
2019-05-11 14:35:01 +0900

    refactor: define namespace

--- index.d.ts
+++ index.d.ts
@@ -1,43 +1,39 @@
-interface OS {
-  architecture: string;
-  family: string;
-  toString(): string;
-  version: string;
-}
+declare namespace NuxtUserAgent {
+  interface OS {
+    architecture: string;
+    family: string;
+    toString(): string;
+    version: string;
+  }
 
-interface Platform {
-  description: string;
-  layout: string;
-  manufacturer: string;
-  name: string;
-  os: OS;
-  parse(): Platform;
-  prerelease: string;
-  product: string;
-  toString(): string;
-  ua: string;
-  version: string;
-}
+  interface Platform {
+    description: string;
+    layout: string;
+    manufacturer: string;
+    name: string;
+    os: OS;
+    parse(): Platform;
+    prerelease: string;
+    product: string;
+    toString(): string;
+    ua: string;
+    version: string;
+  }
 
-export interface NuxtUserAgent {
-  readonly value: string;
-  is: {
-    readonly chrome: boolean;
-    readonly firefox: boolean;
-    readonly safari: boolean;
-    readonly ie: boolean;
-    readonly edge: boolean;
-    readonly opera: boolean;
-    readonly android: boolean;
-    readonly ios: boolean;
-    readonly macos: boolean;
-    readonly windows: boolean;
-    readonly linux: boolean;
-  };
-}
-
-declare module 'vue/types/vue' {
-  export interface Vue {
-    $ua: NuxtUserAgent;
+  interface UserAgent {
+    readonly value: string;
+    is: {
+      readonly chrome: boolean;
+      readonly firefox: boolean;
+      readonly safari: boolean;
+      readonly ie: boolean;
+      readonly edge: boolean;
+      readonly opera: boolean;
+      readonly android: boolean;
+      readonly ios: boolean;
+      readonly macos: boolean;
+      readonly windows: boolean;
+      readonly linux: boolean;
+    };
   }
 }
