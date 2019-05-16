41c25508637b320782996be41265e70736bd5a21
1000ch <shogo.sensui@gmail.com>
2019-05-16 18:30:49 +0900

    refactor: use types/platform

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,20 @@
-declare namespace NuxtUserAgent {
-  interface OS {
-    architecture: string;
-    family: string;
-    toString(): string;
-    version: string;
-  }
+import Platform from 'platform';
 
-  interface Platform {
-    description: string;
-    layout: string;
-    manufacturer: string;
-    name: string;
-    os: OS;
-    parse(): Platform;
-    prerelease: string;
-    product: string;
-    toString(): string;
-    ua: string;
-    version: string;
-  }
-
-  interface UserAgent {
-    readonly platform: Platform;
-    readonly is: {
-      readonly chrome: boolean;
-      readonly firefox: boolean;
-      readonly safari: boolean;
-      readonly ie: boolean;
-      readonly edge: boolean;
-      readonly opera: boolean;
-      readonly android: boolean;
-      readonly ios: boolean;
-      readonly macos: boolean;
-      readonly windows: boolean;
-      readonly linux: boolean;
-    };
-  }
+interface NuxtUserAgent {
+  readonly platform: Platform;
+  readonly is: {
+    readonly chrome: boolean;
+    readonly firefox: boolean;
+    readonly safari: boolean;
+    readonly ie: boolean;
+    readonly edge: boolean;
+    readonly opera: boolean;
+    readonly android: boolean;
+    readonly ios: boolean;
+    readonly macos: boolean;
+    readonly windows: boolean;
+    readonly linux: boolean;
+  };
 }
 
 export default NuxtUserAgent;
